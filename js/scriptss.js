4044beed91106a6243b729815985a45a1e921f60
MosesNkrumah <moses.nkrumah@scynett-ghana.com>
2020-10-27 12:57:51 +0000

    not again

--- js/scriptss.js
+++ js/scriptss.js
@@ -233,7 +233,7 @@ jQuery(window).on('load', function () {
     if (isValidEmail(email) && message.length > 1 && name.length > 1) {
       $.ajax({
         type: 'POST',
-        url: 'http://rebranding.scynetthotspot.com/php/sendmail.php',
+        url: 'https://scynett.com/php/sendmail.php',
         data: dataString,
         success: function () {
           $('#contact-form .success').fadeIn(1000);
